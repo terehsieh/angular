aa175dec9aa12794cf98f49ab2a9ef67f72e62ea
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-10-26 00:39:10 -0300

    creado de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  //  name = 'Angular ' + VERSION.major;
+  title= 'AppTudai';
 }
